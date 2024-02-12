9409bc9d3f5d7aee53087fd50138dd53505fe069
(Student  C00283484) Jekaterina Gorska <C00283484@setu.ie>
2024-02-12 14:53:09 +0000

    added cube

--- PRACTICAL_04/src/Game.cpp
+++ PRACTICAL_04/src/Game.cpp
@@ -9,11 +9,72 @@ Game::~Game() {}
 
 
 // Vertices for one Triangle
-float vertices[] = { 1.0f, 1.0f, -5.0f, -1.0f, 1.0f, -5.0f, -1.0f, -1.0f, -5.0f };
+float vertices[] = {
+					//front face
+					 1.0f, 1.0f, 1.0f, 
+					1.0f, -1.0f, 1.0f, 
+					1.0f, 1.0f, 1.0f,
+					-1.0f, 1.0f, 1.0f,
+					//back face
+					-1.0f, -1.0f, -1.0f,
+					-1.0f, 1.0f, -1.0f,
+					1.0f, 1.0f, -1.0f,
+					1.0f, -1.0f, -1.0f,
+					//top face
+					-1.0f, 1.0f, -1.0f,
+					-1.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, -1.0f,
+					//bottom face
+					-1.0f, -1.0f, -1.0f,
+					1.0f, -1.0f, -1.0f,
+					1.0f, -1.0f, 1.0f,
+					-1.0f, -1.0f, 1.0f,
+					//right face
+					1.0f, -1.0f, -1.0f,
+					1.0f, 1.0f, -1.0f,
+					1.0f, 1.0f, 1.0f, 
+					1.0f, -1.0f, 1.0f,
+					//left face
+					-1.0f, -1.0f, -1.0f,
+					-1.0f, -1.0f, 1.0f,
+					-1.0f, 1.0f, 1.0f,
+					-1.0f, 1.0f, -1.0f};
 
 // Colors for those vertices
-float colors[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
-
+float colors[] = { 
+					//front face
+					1.0f, 0.0f, 0.0f, 1.0f, 
+					0.0f, 1.0f, 0.0f, 1.0f,
+					0.0f, 0.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 0.0f, 1.0f,
+					//back face
+					1.0f, 0.0f, 1.0f, 1.0f,
+					0.0f, 1.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 1.0f, 1.0f,
+					1.0f, 0.0f, 1.0f, 1.0f,
+					//top face
+					0.0f, 1.0f, 0.0f, 1.0f,
+					0.0f, 1.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 0.0f, 1.0f,
+					//bottom face
+					1.0f, 0.5f, 0.0f, 1.0f,
+					1.0f, 0.5f, 1.0f, 1.0f,
+					0.5f, 0.5f, 1.0f, 1.0f,
+					0.5f, 0.5f, 0.0f, 1.0f,
+					//right face
+					1.0f, 0.0f, 1.0f, 1.0f,
+					1.0f, 1.0f, 1.0f, 1.0f,
+					0.0f, 1.0f, 1.0f, 1.0f,
+					0.0f, 0.0f, 1.0f, 1.0f,
+					//left face
+					0.0f, 0.0f, 0.0f, 1.0f,
+					1.0f, 0.0f, 0.0f, 1.0f,
+					1.0f, 1.0f, 0.0f, 1.0f,
+					0.0f, 1.0f, 0.0f, 1.0f };
+					
+					 
 // Index to be drawn
 unsigned int vertex_index[] = { 0, 1, 2 };
 
@@ -70,7 +131,13 @@ void Game::render()
 	glVertexPointer(3, GL_FLOAT, 0, &vertices);
 	glColorPointer(3, GL_FLOAT, 0, &colors);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glEnable(GL_DEPTH_TEST);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -5.0f);
+	glRotatef(0.1f, 1.0f, 1.0f, 0.0f);
+
+	glDrawArrays(GL_QUADS, 0, 24);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, &vertex_index);
 
